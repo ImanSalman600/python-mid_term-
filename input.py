786a8f18e560c70a0a3e637e20a9e3961021a258
ImanSalman600 <156664192+ImanSalman600@users.noreply.github.com>
2024-01-15 15:38:46 +0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+#x=str(input("enter your name"))
+#print("my name is ",x)
+x=str(input("enter 1st number"))
+y=str(input("enter 2nd number"))
+z=x+y
+d=x-y
+print("Add value" , z )
+
+      
+      
